6b4d1f3b9ac775800d83276a3ff018fd99a5c024
Radek Kargul <42668536+rkargul@users.noreply.github.com>
2021-10-01 19:02:38 +0200

    Create searching_algorithm_linear.java

---
+++ searching_algorithm_linear.java
@@ -0,0 +1,17 @@
+
+// Java program to implement Linear Search
+ 
+class searching_algorithm_linear {
+  
+    public static int search(int arr[], int x)
+    {
+        int n = arr.length;
+ 
+        for (int i = 0; i < n; i++) {
+ 
+            if (arr[i] == x)
+                return i;
+        }
+        return -1;
+    }
+}
